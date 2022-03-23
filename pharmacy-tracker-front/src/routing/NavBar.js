746b229a956649e0b0f4571d96cb1f6a1413d4e6
Cristina Falcon <cristinafalconcarque@gmail.com>
2022-03-23 12:24:36 +0000

    Added NavBar and Routing

---
+++ pharmacy-tracker-front/src/routing/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Routing from './Routing';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <ul className="nav justify-content-end">
+        <li className="nav-item">
+          <Link className="nav-link" to={Routing.pharmacies}>
+            My profile
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to={Routing.signInPharmacy}>
+            Sign in
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export {NavBar};
