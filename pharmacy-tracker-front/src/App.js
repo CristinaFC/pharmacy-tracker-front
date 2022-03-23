a54b3b748f8622857af775f9f6a92cbb00a2adf7
Cristina Falcon <cristinafalconcarque@gmail.com>
2022-03-23 12:23:00 +0000

    no message

---
+++ pharmacy-tracker-front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PharmaciesContainer from './pharmacies/pharmacyAccountForm';
+import RouterComponent from './routing/Router';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
