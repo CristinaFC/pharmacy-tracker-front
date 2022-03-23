9e142885cab2e4140f9e775d6055d0f2eea5bd26
Cristina Falcon <cristinafalconcarque@gmail.com>
2022-03-23 12:26:32 +0000

    Pharmacy Component Get a pharmacy by document id and show details

---
+++ pharmacy-tracker-front/src/pharmacies/pharmacy.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Pharmacy {
+    constructor(address, city, location, owner, phone, eClosing, eOpening, mClosing,mOpening, nPharmacy){
+        this.address = address;
+        this.city = city;
+        this.location = location;
+        this.owner = owner;
+        this.phone = phone;
+        this.eClosing = eClosing;
+        this.eOpening = eOpening;
+        this.mClosing = mClosing;
+        this.mOpening = mOpening;
+        this.nPharmacy = nPharmacy;
+    }
+
+}
+
+export const pharmacyConverter = {
+    toFirestore: (pharmacies) => {
+        return {
+            Address: pharmacies.address,
+            City: pharmacies.city,
+            Location: pharmacies.location,
+            Owner: pharmacies.owner,
+            Phone: pharmacies.phone,
+            eClosing: pharmacies.eClosing,
+            eOpening: pharmacies.eOpening,
+            mClosing: pharmacies.mClosing,
+            mOpening: pharmacies.mOpening,
+            nPharmacy: pharmacies.nPharmacy,
+        };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        console.log('data',data);
+        return new Pharmacy(data.Address, data.City, data.Location, data.Owner, data.Phone, data.eClosing, data.eOpening, data.mClosing, data.mOpening, data.nPharmacy);
+    }
+};
+
+export default Pharmacy;
---
+++ pharmacy-tracker-front/src/pharmacies/pharmacyProfile.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { db } from '../firebase/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore/lite';
+import { pharmacyConverter } from './pharmacy';
+
+class PharmacyProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            datos: []
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            //Obtengo los datos de la BD que lo convierte a objeto Pharmacy (en este caso la farmacia llamada 1)
+            const docRef = doc(db, "pharmacies", "1").withConverter(pharmacyConverter);
+            const docSnap = await getDoc(docRef);
+            const pharmacy = docSnap.data();
+
+            this.setState({
+                datos: pharmacy,
+            })
+
+            if (docSnap.exists()) {
+                console.log("Document data:",pharmacy);
+            } else {
+                console.log("No such document!");
+            }
+        } catch (e) {
+        console.error("Error adding document: ", e);
+        }
+    }
+    
+    render() {
+        const {datos} = this.state;
+        
+        return(
+            <div>
+                <ul>
+                    <li>Nº Pharmacy: {datos.nPharmacy}</li>
+                    <li>Address: {datos.address}</li>
+                    <li>City: {datos.city}</li>
+                    <li>Owner: {datos.owner}</li>
+                    <li>Phone: {datos.phone}</li>
+                    <li>Morning Opening: {datos.mOpening}</li>
+                    <li>Morning Closing: {datos.mClosing}</li>
+                    <li>Evening Opening: {datos.eOpening}</li>
+                    <li>Evening Closing: {datos.eClosing}</li>
+                </ul>
+            </div>
+        );
+    };    
+}
+
+
+
+export default PharmacyProfile;
+
+
+
+
