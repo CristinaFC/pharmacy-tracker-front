49af9f0c61bde1c24bcc76c01802fc378130432c
Ayoze <AyozeMN@hotmail.com>
2022-05-03 15:02:02 +0100

    onClick failing

--- pharmacy-tracker-front/src/components/PharmaciesPagination.js
+++ pharmacy-tracker-front/src/components/PharmaciesPagination.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import { getPharmacies } from '../database/functions';
+import { increment } from 'firebase/firestore';
 
 export default function PaginationControlled() {
 
     const [amount, setAmount] = React.useState();
+    const [page, setPage] = React.useState();
 
     async function getPharmaciesNumber() {
         try {
@@ -17,9 +19,24 @@ export default function PaginationControlled() {
 
     getPharmaciesNumber();
 
+    const handleChangePage = (page) => {
+        console.log(page);
+        setPage(page);
+    };
+
     return (
+        /*
+        var currentPage = 1;
+        var maxPages = amont;
+        pharmacies[currentPage-1, currentPage+2];
+        nextpage.onclik(currentpageincrement)
+        f increment()
+        ifcuaosdj 
+        */
         <Pagination 
             count={amount%4 > 0 ? Math.round(amount/4) +1 : amount/4}
+            page={page}
+            onClick={handleChangePage}
             defaultPage={1} 
             siblingCount={0}
             size="small"
