ed544c85a4430871d5cf64affbe02d65192eba25
Cristina Falcon <cristinafalconcarque@gmail.com>
2022-05-03 14:16:46 +0100

    Deleted comments

--- pharmacy-tracker-front/src/routing/NavBar.js
+++ pharmacy-tracker-front/src/routing/NavBar.js
@@ -1,140 +1,45 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Routing from './Routing';
-import { db , auth } from '../firebase/firebaseConfig';
-import { useAuth } from '../context/authContext';
-import { getDoc, doc, collection } from "firebase/firestore/lite";
+import { auth } from '../firebase/firebaseConfig';
 import { getAuth } from 'firebase/auth';
 import { getUserData } from '../database/functions'
 
 
-// class NavBar extends Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       user: []
-//     }
-//   }
-
-//   async componentDidMount() {
-
-//     const id = getAuth().currentUser.uid;
-//     console.log(id);
-//     if(id != null){
-//       const data =  await getUserData();
-//       console.log(data);
-//       this.setState({user: data});
-//     }
-    
-//   }
-
-//   render(){
-
-//     const {user} = this.state;
-//     if(user !== undefined){
-
-//       if (user.role == "normal_user") {
-//         console.log('DENTRO');
-//         return (
-//         <div>
-//           <NavBarAuthUser/>
-//         </div>
-//         );
-//       }else{
-//             return (
-//               <div>
-//                 <NavBarPharmacy/>
-//               </div>
-//               );
-//       }
-//     }
-
-   
-//     return (
-//       <div>
-//         {<NavBarNonAuthUser/>}
-//       </div>
-//       );
-//   }
-  
-// }
-
 export function NavBar()
 {
   const [login, setLogin] = useState();
-  const [user, setUser] = useState();
-  try {
+
+  try
+  {
     const userRef = getAuth().currentUser.uid;
-    getUserData(userRef).then((data) => {
-      if (data.role == "normal_user") {
+    getUserData(userRef).then((data) =>
+    {
+      if (data.role == "normal_user")
+      {
         setLogin(true);
-      } else if (data.role == "pharmacy") {
+      } else if (data.role == "pharmacy")
+      {
         setLogin(false);
       }
     })
-  } catch(e) {
+  } catch (e)
+  {
     console.log(e);
   }
 
   const isLogged = getAuth().currentUser;
-  
-  // getUserData(user).then((data) => {
-  //   if (data.role == "normal_user") {
-  //     console.log(data);
-  //     setLogin(true);
-  //     return (
-  //     <div>
-  //       <NavBarAuthUser/>
-  //     </div>
-  //     );
-  //   }else{
-  //         return (
-  //           <div>
-  //             <NavBarPharmacy/>
-  //           </div>
-  //           );
-  //   }
-  //     }).catch(e => {
-  //       console.error(e);
-  //     });
-    //console.log(data);
- 
 
 
-      // if(isLogged) {
-      //   console.log('here');
-      //   return (
-      //     <div>
-      //       {nUser === true ? <NavBarAuthUser/> : <NavBarPharmacy/>}
-      //     </div>
-      
-      //   );
-      // }
-  console.log("Llego");
   return (
     <div>
-      {isLogged == null ? <NavBarNonAuthUser/> : login == true ? <NavBarAuthUser/> : <NavBarPharmacy/>}
+      {isLogged == null ? <NavBarNonAuthUser /> : login == true ? <NavBarAuthUser /> : <NavBarPharmacy />}
     </div>
-    );
+  );
 
-  // var status = async function () {
-  //   const user = getAuth().currentUser.uid;
-  //   const data = getUserData(user);
-  //   status = await data.role;
-  //   console.log(status);
-  // }
-  
-  // const getUser = async () => {
-  //   let doc = null;
-  //   const userRef = db.collection('users').doc(user.uid);
-  //   doc = await userRef.get();
-  //   console.log(doc);
-  // }
-  
 }
 
 const NavBarNonAuthUser = () =>
@@ -147,10 +52,6 @@ const NavBarNonAuthUser = () =>
           <strong class="title">Pharmacy Tracker</strong>
         </Link>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          {/* <form class="ms-5 d-flex form-search">
-            <input type="search" class="form-control" placeholder="Search" aria-label="Search"
-            />
-          </form> */}
           <ul class="navbar-nav ms-5">
             <li class="nav-item top-r" >
               <Link class="nav-login" to={Routing.login}>Login</Link>
@@ -199,7 +100,7 @@ const NavBarPharmacy = () =>
 
 const NavBarAuthUser = () =>
 {
-console.log('DENTRO');
+  console.log('DENTRO');
   return (
     <nav class="navbar navbar-expand-lg">
       <div class="container">
@@ -210,9 +111,6 @@ console.log('DENTRO');
           <li class="nav-item" >
             <Link class="nav-profile" to={Routing.userProfile}>Profile</Link>
           </li>
-          {/* <li class="nav-item" >
-            <Link class="nav-products" to={Routing.myProducts}>Products</Link>
-          </li> */}
         </ul>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
@@ -220,9 +118,7 @@ console.log('DENTRO');
             <li class="nav-item" >
               <Link class="nav-logout" onClick={() => auth.signOut()} to={Routing.home}>Log out</Link>
             </li>
-            <li>
-              <a> Your role is: Normal User </a>
-            </li>
+
 
           </ul>
         </div>
